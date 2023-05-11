7c21bef6ab83a3dbc145eabc721c309193738a4b
AinsOoalDown <6603505-AinsOoalDown@users.noreply.replit.com>
2023-05-11 04:15:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let colorMode = 'dark';
+
+function toggleColors() {
+	const button = document.querySelector('.color-toggle');
+	document.body.classList.toggle('light-mode');
+	if (colorMode === 'dark') {
+		colorMode = 'light';
+		button.textContent = 'Dark Mode';
+	} else {
+		colorMode = 'dark';
+		button.textContent = 'Light Mode';
+	}
+}
